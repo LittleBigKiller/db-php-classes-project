021bfe8f2f292cbaa9345455c2b761f8364079fb
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2019-11-17 16:47:53 +0100

    [2019-11-07_-_Projekt] Top Users i Top Questions dodane

--- 2019-11-07_-_Projekt/as-projekt/highscores.php
+++ 2019-11-07_-_Projekt/as-projekt/highscores.php
@@ -1,23 +1,53 @@
-<?php
-include('dbcredentials.php');
-$conn = new mysqli($db_server, $db_user, $db_passwd, $db_dbname);
+<!DOCTYPE html>
+<html lang="en">
 
-if ($conn->connect_errno) die('Brak połączenia');
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>10 najlepszych użytkowników</title>
+</head>
 
-$rs = $conn->query('SELECT `users`.`username` AS "username", AVG(`results`.`ans_correct`) AS "avg" FROM `results` INNER JOIN `users` ON `results`.`uid` = `users`.`uid` GROUP BY `results`.`uid` ORDER BY AVG(`results`.`ans_correct`) DESC LIMIT 10')
-    or die('Błąd pobierania danych');
+<body>
+    <?php
+    include('dbcredentials.php');
+    $conn = new mysqli($db_server, $db_user, $db_passwd, $db_dbname);
 
-if ($rs->num_rows > 0) {
+    if ($conn->connect_errno) die('Brak połączenia');
+
+    echo '
+        <h2> 10 najlepszych użytkowników
+            <form action="/as-projekt/" method="GET">
+                <button>Wróć do strony głównej</button>
+            </form>
+        </h2>
+        ';
+
+    $rs = $conn->query('SELECT `users`.`username` AS "username", AVG(`results`.`ans_correct`) AS "avg" FROM `results` INNER JOIN `users` ON `results`.`uid` = `users`.`uid` GROUP BY `results`.`uid` ORDER BY AVG(`results`.`ans_correct`) DESC LIMIT 10')
+        or die('Błąd pobierania danych 2');
+
+
     echo '<table border=1>
-		<tr><th>Użytkownik</th><th>Średnia Ocen</th></tr>';
-    while ($rec = $rs->fetch_array()) {
-        echo "<tr>
-			<td>" . $rec["username"] . "</td>
-			<td>" . ($rec["avg"] * 10) . "%</td>
-			</tr>";
-    }
-} else {
-    echo 'no data';
-}
+        <tr><th>Użytkownik</th><th>Średnia Ocen</th></tr>';
 
-?>
+    if ($rs->num_rows > 0) {
+        while ($rec = $rs->fetch_array()) {
+            echo '
+                <tr>
+                    <td>' . $rec['username'] . '</td>
+                    <td>' . ($rec['avg'] * 10) . '%</td>
+                </tr>';
+        }
+    } else {
+        echo '
+            <tr>
+                <td colspan="2">Brak Danych</td>
+            </tr>';
+    }
+
+    echo '</table>';
+
+    ?>
+</body>
+
+</html>

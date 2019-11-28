9289891d6b3600bcd53c09ab1abbb96042876c21
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2019-11-28 11:43:43 +0100

    [2019-11-07_-_Projekt] Ostylowanie login.php

--- 2019-11-07_-_Projekt/as-projekt/login.php
+++ 2019-11-07_-_Projekt/as-projekt/login.php
@@ -27,64 +27,89 @@ if (isset($_POST['login'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <?php
-    $passlogin = false;
+    <div id="header">
+        <div id="header_helper"></div>
+        <h1> Logowanie </h1>
+        <div id="header_helper"></div>
+    </div>
+    <div id="main_box">
+        <div id="logreg_content_box">
+            <?php
+            $passlogin = false;
 
-    if (isset($_POST['login'])) {
-        include('dbcredentials.php');
-        $conn = new mysqli($db_server, $db_user, $db_passwd, $db_dbname);
+            if (isset($_POST['login'])) {
+                include('dbcredentials.php');
+                $conn = new mysqli($db_server, $db_user, $db_passwd, $db_dbname);
 
-        if ($conn->connect_errno) die('Nie można się połączyć z bazą danych');
+                if ($conn->connect_errno) die('Nie można się połączyć z bazą danych');
 
-        $rs = $conn->query('SELECT `perm_level` FROM `users` WHERE `username` = "' . $username . '" AND `password` = "' . md5($_POST['password']) . '"')
-            or die('Błąd pobierania danych');
+                $rs = $conn->query('SELECT `perm_level` FROM `users` WHERE `username` = "' . $username . '" AND `password` = "' . md5($_POST['password']) . '"')
+                    or die('Błąd pobierania danych');
 
-        if ($rs->num_rows > 0) {
-            $passlogin = true;
-        }
-    }
+                if ($rs->num_rows > 0) {
+                    $passlogin = true;
+                }
+            }
 
-    if ($passlogin) {
-        echo '
-            <h1>Zalogowano: ' . $_POST['username'] . '</h1>
-            <form action="/as-projekt/" method="GET">
-                <button>Wróć do strony głównej</button>
-            </form>';
-    } else if (isset($_COOKIE['username']) and isset($_COOKIE['password'])) {
-        echo '
-            <h1>Zalogowano: ' . str_replace('\\"', '"', $_COOKIE['username']) . '</h1>
-            <form action="/as-projekt/" method="GET">
-                <button>Wróć do strony głównej</button>
-            </form>';
-    } else {
-        if (isset($_POST['login'])) {
-            echo '
-                <h1>Nieprawidłowe dane logowania</h1>
-                <form action="/as-projekt/login.php" method="GET">
-                    <button>Wróć do logowania</button>
-                </form>';
-        } else {
-            echo '
-                <form method="POST">
-                    <input type="hidden" name="login" value="true">
-                    <input type="text" maxlength=15 name="username" required>
-                    <label for="username">Username</label>
-                    <br>
-                    <input type="password" maxlength=16 name="password" required>
-                    <label for="password">Password</label>
-                    <br>
-                    <button>Zaloguj</button>
-                </form>
+            if ($passlogin) {
+                echo '<h1>Zalogowano: ' . $_POST['username'] . '</h1>';
+                ?>
                 <form action="/as-projekt/" method="GET">
-                    <button>Wróć do strony głównej</button>
+                    <button class="button_menu">Wróć do strony głównej</button>
                 </form>
-                ';
-        }
-    }
-    ?>
+            <?php
+            } else if (isset($_COOKIE['username']) and isset($_COOKIE['password'])) {
+                echo '<h1>Zalogowano: ' . str_replace('\\"', '"', $_COOKIE['username']) . '</h1>';
+                ?>
+                <form action="/as-projekt/" method="GET">
+                    <button class="button_menu">Wróć do strony głównej</button>
+                </form>
+                <?php
+                } else {
+                    if (isset($_POST['login'])) {
+                        ?>
+                    <h1>Nieprawidłowe dane logowania</h1>
+                    <form action="/as-projekt/login.php" method="GET">
+                        <button class="button_menu">Wróć do strony logowania</button>
+                    </form>
+                <?php
+                    } else {
+                        ?>
+                    <div id="input_cont">
+                        <h1>Zaloguj</h1>
+                        <form class="big_form" method="POST">
+                            <input type="hidden" name="login" value="true">
+                            <div class="input_elem">
+                                <label for="username">Username</label>
+                                <input type="text" maxlength=15 name="username" required>
+                            </div>
+                            <div class="input_elem">
+                                <label for="password">Hasło</label>
+                                <input type="password" maxlength=16 name="password" required>
+                            </div>
+                            <button class="button_menu">Zaloguj</button>
+                        </form>
+                        <form action="/as-projekt/" method="GET">
+                            <button class="button_menu">Wróć do strony głównej</button>
+                        </form>
+                    </div>
+            <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
+
+    <div id="footer">
+        <div id="footer_text">
+            Projekt - 2019-11-07
+        </div>
+    </div>
 </body>
 
 </html>
